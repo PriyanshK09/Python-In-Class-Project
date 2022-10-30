5b95e8b304e7afdd1a003e341d9069626aff2394
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-10-30 19:13:58 +0530

    modified:   main7.py

--- main7.py
+++ main7.py
@@ -6,3 +6,15 @@ for num in lst1:
     if num % 3 == 0:
         total += num
 print(total)
+
+#Using "while" loop
+
+total = 0
+nums = [1, 3, 4, 6, 8, 9, 11, 13, 15, 16, 17, 18]
+index = 0
+while index < len(nums):
+    num = nums[index]
+    if num % 3 == 0:
+        total += num
+    index = index + 1
+print(total)
