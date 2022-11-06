9f45f11a9f1b1817851f8621b750cbfff42af126
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-06 16:48:19 +0530

    new file:   main13.py new file:   main14.py

---
+++ main13.py
@@ -0,0 +1,7 @@
+def myFunction(num1):
+    return num1*num1
+
+x = int(input("Enter a number : "))
+myFunction(x)
+prod = myFunction
+print(prod)
---
+++ main14.py
@@ -0,0 +1,9 @@
+import  random
+
+def randombb():
+        a = random.random()
+        print(a)
+        return a
+
+x= randombb()
+print(x)
